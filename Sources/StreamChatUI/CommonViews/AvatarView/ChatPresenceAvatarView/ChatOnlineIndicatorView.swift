08db97df4679fee4a4a7f12d5c4dcde2c78ac1c6
Jakub Skořepa <skorepak@gmail.com>
2021-03-30 14:29:06 +0200

    Mask indicator view in avatar view

--- Sources/StreamChatUI/CommonViews/AvatarView/ChatPresenceAvatarView/ChatOnlineIndicatorView.swift
+++ Sources/StreamChatUI/CommonViews/AvatarView/ChatPresenceAvatarView/ChatOnlineIndicatorView.swift
@@ -10,27 +10,37 @@ public typealias ChatOnlineIndicatorView = _ChatOnlineIndicatorView<NoExtraData>
 
 /// A view used to indicate the presence of a user.
 open class _ChatOnlineIndicatorView<ExtraData: ExtraDataTypes>: _View, UIConfigProvider {
+    
+    open private(set) lazy var circleView: UIView = UIView()
+        .withoutAutoresizingMaskConstraints
+    
     override public func defaultAppearance() {
         super.defaultAppearance()
 
-        backgroundColor = uiConfig.colorPalette.alternativeActiveTint
-        layer.borderColor = uiConfig.colorPalette.lightBorder.cgColor
+        backgroundColor = .clear
+        circleView.backgroundColor = uiConfig.colorPalette.alternativeActiveTint
     }
 
     override open func setUpLayout() {
         super.setUpLayout()
         heightAnchor.pin(equalTo: widthAnchor).isActive = true
+        
+        let borderWidth: CGFloat = 2
+        embed(circleView, insets: .init(top: borderWidth, leading: borderWidth, bottom: borderWidth, trailing: borderWidth))
     }
 
     override open func layoutSubviews() {
         super.layoutSubviews()
+        
         layer.cornerRadius = bounds.width / 2
-        layer.borderWidth = (bounds.width / 5)
         layer.masksToBounds = true
-
-        // Create a circle shape layer with true bounds.
-        let mask = CAShapeLayer()
-        mask.path = UIBezierPath(ovalIn: bounds.inset(by: .init(top: -1, left: -1, bottom: -1, right: -1))).cgPath
-        layer.mask = mask
+        
+        circleView.layer.cornerRadius = circleView.bounds.width / 2
+        circleView.layer.masksToBounds = true
+    }
+    
+    /// Path used to mask space in super view. 
+    open var maskingPath: CGPath? {
+        UIBezierPath(ovalIn: frame).cgPath
     }
 }
--- Sources/StreamChatUI/CommonViews/AvatarView/ChatPresenceAvatarView/ChatPresenceAvatarView.swift
+++ Sources/StreamChatUI/CommonViews/AvatarView/ChatPresenceAvatarView/ChatPresenceAvatarView.swift
@@ -16,9 +16,17 @@ open class _ChatPresenceAvatarView<ExtraData: ExtraDataTypes>: _View, UIConfigPr
         .withoutAutoresizingMaskConstraints
 
     /// A view indicating whether the user this view represents is online.
-    open private(set) lazy var onlineIndicatorView: UIView = uiConfig
+    open private(set) lazy var onlineIndicatorView: _ChatOnlineIndicatorView<ExtraData> = uiConfig
         .onlineIndicatorView.init()
         .withoutAutoresizingMaskConstraints
+    
+    /// Bool to determine if the indicator should be shown.
+    open var showOnlineIndicator: Bool = false {
+        didSet {
+            onlineIndicatorView.isVisible = showOnlineIndicator
+            setUpMask(indicatorVisible: showOnlineIndicator)
+        }
+    }
 
     override public func defaultAppearance() {
         super.defaultAppearance()
@@ -34,5 +42,27 @@ open class _ChatPresenceAvatarView<ExtraData: ExtraDataTypes>: _View, UIConfigPr
         onlineIndicatorView.widthAnchor
             .pin(equalTo: widthAnchor, multiplier: 0.3)
             .isActive = true
+    }
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        
+        setUpMask(indicatorVisible: showOnlineIndicator)
+    }
+    
+    /// Creates space for indicator view in avatar view by masking path provided by the indicator view.
+    /// - Parameter visible: Bool to determine if the indicator should be shown. The avatar view won't be masked if the indicator is not visible.
+    open func setUpMask(indicatorVisible: Bool) {
+        guard
+            indicatorVisible,
+            let path = onlineIndicatorView.maskingPath?.mutableCopy()
+        else { return avatarView.layer.mask = nil }
+        
+        path.addRect(bounds)
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = path
+        maskLayer.fillRule = .evenOdd
+
+        avatarView.layer.mask = maskLayer
     }
 }
