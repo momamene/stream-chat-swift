e4b7bff5262be23eb1222efd242ed354d223b425
Vojta Stavik <stavik@outlook.com>
2021-04-01 02:38:29 +0200

    Add `session` parameter to `EventMiddleware`

--- Sources/StreamChat/WebSocketClient/EventMiddlewares/EventMiddleware.swift
+++ Sources/StreamChat/WebSocketClient/EventMiddlewares/EventMiddleware.swift
@@ -11,33 +11,35 @@ protocol EventMiddleware {
     ///
     /// - Parameters:
     ///   - event: The incoming `Event`.
+    ///   - session: The database session the middleware works with.
     ///   - completion: Called when the event processing is done. If called with `nil`, no middlewares down the
     ///   chain are called.
-    func handle(event: Event, completion: @escaping (Event?) -> Void)
+    func handle(event: Event, session: DatabaseSession, completion: @escaping (Event?) -> Void)
 }
 
 extension Array where Element == EventMiddleware {
     /// Evaluates an array of `EventMiddleware`s in the order they're specified in the array. It's not guaranteed that
-    /// all middlewares are called. If a middleware returns `nil`, no middlewares down in the chain are called.
+    /// all middlewares are called. If a middleware calls the completion with `nil`, no middlewares down in the chain are called.
     ///
     /// - Parameters:
     ///   - event: The event to be pre-processed.
+    ///   - session: The database session used when evaluating the middlewares.
     ///   - completion: Called when the event pre-processing is finished. Be aware that `completion` can be called
     ///   multiple times for a single event.
-    func process(event: Event, completion: @escaping (Event?) -> Void) {
+    func process(event: Event, session: DatabaseSession, completion: @escaping (Event?) -> Void) {
         guard isEmpty == false else { completion(event); return }
-        evaluate(idx: startIndex, event: event, completion: completion)
+        evaluate(idx: startIndex, event: event, session: session, completion: completion)
     }
     
-    private func evaluate(idx: Int, event: Event, completion: @escaping (Event?) -> Void) {
+    private func evaluate(idx: Int, event: Event, session: DatabaseSession, completion: @escaping (Event?) -> Void) {
         let middleware = self[idx]
-        middleware.handle(event: event) { event in
+        middleware.handle(event: event, session: session) { event in
             let nextIdx = idx + 1
             if nextIdx == self.endIndex {
                 completion(event)
                 
             } else if let event = event {
-                self.evaluate(idx: nextIdx, event: event, completion: completion)
+                self.evaluate(idx: nextIdx, event: event, session: session, completion: completion)
                 
             } else {
                 completion(nil)
--- Sources/StreamChat/WebSocketClient/EventMiddlewares/EventMiddleware_Tests.swift
+++ Sources/StreamChat/WebSocketClient/EventMiddlewares/EventMiddleware_Tests.swift
@@ -2,16 +2,17 @@
 // Copyright © 2021 Stream.io Inc. All rights reserved.
 //
 
+import CoreData
 @testable import StreamChat
 import XCTest
 
 class EventMiddleware_Tests: XCTestCase {
     /// A test middleware that can be initiated with a closure/
     struct ClosureBasedMiddleware: EventMiddleware {
-        let closure: (_ event: Event, _ completion: @escaping (Event?) -> Void) -> Void
+        let closure: (_ event: Event, _ session: DatabaseSession, _ completion: @escaping (Event?) -> Void) -> Void
         
-        func handle(event: Event, completion: @escaping (Event?) -> Void) {
-            closure(event, completion)
+        func handle(event: Event, session: DatabaseSession, completion: @escaping (Event?) -> Void) {
+            closure(event, session, completion)
         }
     }
     
@@ -21,15 +22,20 @@ class EventMiddleware_Tests: XCTestCase {
     }
     
     func test_middlewareEvaluation() throws {
+        var database: DatabaseContainer! = DatabaseContainerMock()
+        let usedSession = database.viewContext
+        
         let chain: [EventMiddleware] = [
             // Adds `1` to the event synchronously
-            ClosureBasedMiddleware { event, completion in
+            ClosureBasedMiddleware { event, session, completion in
+                XCTAssertEqual(session as! NSManagedObjectContext, usedSession)
                 let event = event as! IntBasedEvent
                 completion(IntBasedEvent(value: event.value + 1))
             },
             
             // Adds `1` to the event synchronously and resets it to `0` asynchronously
-            ClosureBasedMiddleware { event, completion in
+            ClosureBasedMiddleware { event, session, completion in
+                XCTAssertEqual(session as! NSManagedObjectContext, usedSession)
                 let event = event as! IntBasedEvent
                 DispatchQueue.main.async {
                     completion(IntBasedEvent(value: 0))
@@ -40,11 +46,13 @@ class EventMiddleware_Tests: XCTestCase {
         
         // Evaluate the middlewares and record the events
         var result: [IntBasedEvent?] = []
-        chain.process(event: IntBasedEvent(value: 0)) {
+        chain.process(event: IntBasedEvent(value: 0), session: usedSession) {
             result.append($0 as? IntBasedEvent)
         }
         
         // Check we have two callbacks with correct results
         AssertAsync.willBeEqual(result, [IntBasedEvent(value: 2), IntBasedEvent(value: 0)])
+        
+        AssertAsync.canBeReleased(&database)
     }
 }
